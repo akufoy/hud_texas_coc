f77f5b80fe41400ab72e5558bfe8d09e4410c7f4
ak <akufoy@gmail.com>
2018-09-11 13:25:58 -0500

    updating gray scale and austin concentric circles

--- testing/style.js
+++ testing/style.js
@@ -8,20 +8,111 @@ const coc = svg.append("g").attr("class", "coc");
 
 function createMap() {
 
-    d3.json("TX_counties.json", function(error,data) {
+    d3.json("tx_counties_v2.geojson", function(error,data) {
 	if (error) return console.error(error);
 	console.log(data);
-	counties.append("path").datum(topojson.feature(data, data.objects.subunits)).attr("class", "county").attr("d", d3.geo.path().projection(projection));
-	counties.append("path").datum(topojson.mesh(data, data.objects.subunits, function(a,b) { return a !== b;})).attr("d", d3.geo.path().projection(projection)).attr("class", "county-boundary");
+	counties.selectAll("path").data(data.features).enter().append('path')
+	    .attr("class", "county")
+	    .attr("d", d3.geo.path().projection(projection));
+	
+	svg.append("circle").attr("class", "dot")
+	    .attr("transform", "translate(" + projection([-97.7431, 30.2672]) + ")")
+	    .attr("r", 1);
+
+	setInterval(function() {
+	    svg.append("circle")
+		.attr("class", "ring")
+		.attr("transform", "translate(" + projection([-97.7431, 30.2672]) + ")")
+		.attr("r", 1)
+		.style("stroke-width", 1)
+		.style("stroke", "red")
+		.transition()
+		.ease("linear")
+		.duration(6000)
+		.style("stroke-opacity", 1e-6)
+		.style("stroke-width", 1)
+		.style("stroke", "brown")
+		.attr("r", 15)
+		.remove();
+	}, 750);
+	
     });
 
+    d3.json("json/TX_503_subunits.json", function(error, data) {
+	if (error) return console.error(error);
+	// console.log(data);
+
+	coc.append("path").datum(topojson.feature(data, data.objects.subunits)).attr("class", "coc").attr("d", d3.geo.path().projection(projection));
+    });
+    d3.json("json/TX_500", function(error, data) {
+	if (error) return console.error(error);
+	// console.log(data);
+
+	coc.append("path").datum(topojson.feature(data, data.objects.subunits)).attr("class", "coc").attr("d", d3.geo.path().projection(projection));
+    });
+    
     d3.json("json/TX_701.json", function(error, data) {
 	if (error) return console.error(error);
-	console.log(data);
+	// console.log(data);
 
 	coc.append("path").datum(topojson.feature(data, data.objects.subunits)).attr("class", "coc").attr("d", d3.geo.path().projection(projection));
     });
 
+        d3.json("json/TX_600.json", function(error, data) {
+	if (error) return console.error(error);
+	// console.log(data);
 
+	coc.append("path").datum(topojson.feature(data, data.objects.subunits)).attr("class", "coc").attr("d", d3.geo.path().projection(projection));
+    });
+    d3.json("json/TX_601.json", function(error, data) {
+	if (error) return console.error(error);
+	// console.log(data);
+
+	coc.append("path").datum(topojson.feature(data, data.objects.subunits)).attr("class", "coc").attr("d", d3.geo.path().projection(projection));
+    });
+
+    d3.json("json/TX_603.json", function(error, data) {
+	if (error) return console.error(error);
+	// console.log(data);
+
+	coc.append("path").datum(topojson.feature(data, data.objects.subunits)).attr("class", "coc").attr("d", d3.geo.path().projection(projection));
+    });
+   
+        d3.json("json/TX_604.json", function(error, data) {
+	if (error) return console.error(error);
+	// console.log(data);
+
+	coc.append("path").datum(topojson.feature(data, data.objects.subunits)).attr("class", "coc").attr("d", d3.geo.path().projection(projection));
+    });
+
+        d3.json("json/TX_611.json", function(error, data) {
+	if (error) return console.error(error);
+	// console.log(data);
+
+	coc.append("path").datum(topojson.feature(data, data.objects.subunits)).attr("class", "coc").attr("d", d3.geo.path().projection(projection));
+    });
+
+        d3.json("json/TX_624.json", function(error, data) {
+	if (error) return console.error(error);
+	// console.log(data);
+
+	coc.append("path").datum(topojson.feature(data, data.objects.subunits)).attr("class", "coc").attr("d", d3.geo.path().projection(projection));
+    });
+
+        d3.json("json/TX_700.json", function(error, data) {
+	if (error) return console.error(error);
+	// console.log(data);
+
+	coc.append("path").datum(topojson.feature(data, data.objects.subunits)).attr("class", "coc").attr("d", d3.geo.path().projection(projection));
+    });
+
+        d3.json("json/TX_701.json", function(error, data) {
+	if (error) return console.error(error);
+	// console.log(data);
+
+	coc.append("path").datum(topojson.feature(data, data.objects.subunits)).attr("class", "coc").attr("d", d3.geo.path().projection(projection));
+    });
+
+    
 };
 createMap();
